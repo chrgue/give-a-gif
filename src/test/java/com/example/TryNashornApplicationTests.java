8742aaebd1077db8963963266682875a7dba8c5d
Christian G. <chr.gue@gmail.com>
2016-11-17 18:10:25 +0100

    #4: basic integration test

--- src/test/java/com/example/TryNashornApplicationTests.java
+++ src/test/java/com/example/TryNashornApplicationTests.java
@@ -2,15 +2,31 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.chrgue.nashorn.TryNashornApplication;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.RequestSpecBuilder;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = TryNashornApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TryNashornApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Value("${local.server.port}")
+	int port;
 
+	@Test
+	public void testHome() {
+		RestAssured.given()
+			.spec(new RequestSpecBuilder().build())
+			.baseUri("http://localhost")
+			.port(port)
+			.when()
+			.get("/")
+			.then()
+			.statusCode(200);
+	}
 }
