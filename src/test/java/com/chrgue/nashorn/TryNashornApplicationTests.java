49e46b0ac6e13715678ca3bd0621336bdadaa6c1
mle <marcol@web.de>
2016-11-22 21:59:44 +0100

    #4 extend controller/rendering test

--- src/test/java/com/chrgue/nashorn/TryNashornApplicationTests.java
+++ src/test/java/com/chrgue/nashorn/TryNashornApplicationTests.java
@@ -1,5 +1,9 @@
 package com.chrgue.nashorn;
 
+import static com.chrgue.nashorn.RegexMatcher.matches;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,9 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.chrgue.nashorn.TryNashornApplication;
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.builder.RequestSpecBuilder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TryNashornApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -21,12 +23,13 @@ public class TryNashornApplicationTests {
 	@Test
 	public void testHome() {
 		RestAssured.given()
-			.spec(new RequestSpecBuilder().build())
-			.baseUri("http://localhost")
-			.port(port)
-			.when()
-			.get("/")
-			.then()
-			.statusCode(200);
+				.baseUri("http://localhost")
+				.port(port)
+				.when()
+				.get("/")
+				.then()
+				.statusCode(200)
+				.body(not(matches(".*\\{.*\\}.*")))
+				.body("html.body.div.@class", is("container"));
 	}
 }
