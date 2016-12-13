b8a3bfef0628a344622e1187eda3d6cc5ed32ecc
Christian G. <chr.gue@gmail.com>
2016-12-13 14:25:34 +0100

    #6: move bootstrap to package.json & build with webpack

--- src/main/resources/static/jsx/render-client.jsx
+++ src/main/resources/static/jsx/render-client.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {App} from './components/App.jsx'
-import $ from 'jquery';
+
+// loads the bootstrap js
+import 'bootstrap';
+// loads the bootstrap css
+import 'bootstrap/dist/css/bootstrap.css';
 
 var renderClient = function (initialState) {
-	
     ReactDOM.render(
     	<App data={initialState}/>,
         document.getElementById("content")
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: {
-	  server: './src/main/resources/static/jsx/render-server.jsx',
-	  client: './src/main/resources/static/jsx/render-client.jsx',
-  },
-  output: { path: './target/classes/static/js', filename: '[name].app.js' },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
+	entry : {
+		server : './src/main/resources/static/jsx/render-server.jsx',
+		client : './src/main/resources/static/jsx/render-client.jsx',
+	},
+	output : {
+		path : './target/classes/static/',
+		filename : 'js/[name].app.js'
+	},
+	module : {
+		loaders : [
+			{
+				test : /.jsx?$/,
+				loader : 'babel-loader',
+				exclude : /node_modules/,
+				query : {
+					presets : [ 'es2015', 'react' ]
+				}
+			},
+			{
+				test : /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader : 'file'
+			},
+			{
+				test : /\.(woff|woff2)$/,
+				loader : 'url?prefix=font/&limit=5000'
+			},
+			{
+				test : /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader : 'url?limit=10000&mimetype=application/octet-stream'
+			},
+			{
+				test : /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader : 'url?limit=10000&mimetype=image/svg+xml'
+			},
+			// Extract css files
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            }
+		]
+	},
+	plugins : [
+		// some libraries assuming jquery been defined globally (e.g. bootstrap js)  		
+		new webpack.ProvidePlugin({
+			jQuery : 'jquery',
+			$: 'jquery'
+		}),
+		new ExtractTextPlugin("style/[name].css")
+	]
 };
