04ef2174ffe71c2b04ce92bdf0f228d37ffa422e
Christian G. <chr.gue@gmail.com>
2016-12-13 17:04:29 +0100

    #5: publish jmeter html report to github pages

---
+++ publish_load_test_results.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+# run the load tests
+mvn jmeter:jmeter
+
+# create load test results
+mvn jmeter-analysis:analyze
+
+
+cd target/jmeter/results/report
+
+git init
+git config user.name "Christian Günther"
+git config user.email "chr.gue@gmail.com"
+
+git remote add upstream "https://$GITHUB_TOKEN@github.com/chrgue/try-nashorn.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
